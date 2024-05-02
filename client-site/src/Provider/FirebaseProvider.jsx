0d8c4fb3269ea1e1d4a4df25dac56edbeed7e12e
tahfeezmizan <mizanahmed5316@gmail.com>
2024-05-02 22:45:45 +0600

    craete auth context

---
+++ client-site/src/Provider/FirebaseProvider.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+const AuthContext = createContext();
+
+const FirebaseProvider = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FirebaseProvider;
